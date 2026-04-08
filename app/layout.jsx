9d8a00d40db9d0cb4ffd4d20ab1da28f6f967a1a
osmy25 <osmy25@student.bth.se>
2026-04-08 13:11:22 +0200

    plausible script

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,13 @@
 import "./globals.css";
 import styles from "./layout.module.css";
 import Header from "../components/Header/Header";
+import Script from "next/script";
 
 export const metadata = {
-  title: "Stuff My Dog Loves",
+  title: {
+    default: "Stuff My Dog Loves",
+    template: "%s | Stuff My Dog Loves",
+  },
   description: "A pixel art gallery of everything my dog loves.",
 };
 
@@ -11,6 +15,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={styles.body}>
+        
+        {/* Plausible analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://plausible.io/js/pa-irYK1VZ9R2L9KOTP3znEz.js"
+        />
+        <Script id="plausible-init" strategy="afterInteractive">
+          {`
+            window.plausible = window.plausible || function() {
+              (window.plausible.q = window.plausible.q || []).push(arguments)
+            };
+            plausible.init();
+          `}
+        </Script>
+
         <Header />
 
         <main className={styles.main}>
