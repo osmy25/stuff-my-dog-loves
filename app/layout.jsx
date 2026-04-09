7161b0c8ae87f3f2f740a0a0600320b4b05046dc
osmy25 <osmy25@student.bth.se>
2026-04-09 06:15:38 +0200

    defined palette

--- app/layout.jsx
+++ app/layout.jsx
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
           }}
         />
       </head>
-      <body className={styles.body}>
+      <body className={styles.page}>
         <Header />
         <main className={styles.main}>{children}</main>
       </body>
