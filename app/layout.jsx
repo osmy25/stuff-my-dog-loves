8034987a0e83679667ceb348b99b918444164f82
osmy25 <osmy25@student.bth.se>
2026-04-15 06:47:16 +0200

    removed component from preview

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import styles from "./layout.module.css";
+import Script from "next/script";
 import Header from "../components/Header/Header";
 
 export const metadata = {
@@ -13,24 +14,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          async
-          src="https://plausible.io/js/pa-irYK1VZ9R2L9KOTP3znEz.js"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
-              plausible.init()
-            `,
-          }}
-        />
-      </head>
       <body className={styles.page}>
         <Header />
         <main className={styles.main}>{children}</main>
       </body>
+
+      <Script
+        src="https://plausible.io/js/pa-irYK1VZ9R2L9KOTP3znEz.js"
+        strategy="afterInteractive"
+      />
+
+      <Script id="plausible-init" strategy="afterInteractive">
+        {`
+          window.plausible = window.plausible || function () {
+            (window.plausible.q = window.plausible.q || []).push(arguments);
+          };
+          window.plausible.init = window.plausible.init || function (i) {
+            window.plausible.o = i || {};
+          };
+          window.plausible.init();
+        `}
+      </Script>
     </html>
   );
 }
