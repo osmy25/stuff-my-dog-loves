2ee3f0814069cd00a57b34bc646b19c7013bd867
osmy25 <osmy25@student.bth.se>
2026-06-06 07:45:15 +0200

    updated map click popups to center, and added some more details to map (squirrel, birds, mailbox)

--- app/map/MapClient.jsx
+++ app/map/MapClient.jsx
@@ -6,13 +6,13 @@ import styles from "./page.module.css";
 export default function MapClient() {
   const [thought, setThought] = useState(null);
 
-  function showThought(text, x, y) {
-    if (thought?.text === text) {
+  function showThought(text) {
+    if (thought === text) {
       setThought(null);
       return;
     }
 
-    setThought({ text, x, y });
+    setThought(text);
   }
 
   return (
@@ -26,16 +26,21 @@ export default function MapClient() {
         draggable={false}
       />
 
+      {/* mailbox */}
+      <button
+        className={`${styles.hotspot} ${styles.mailbox}`}
+        onClick={(e) => {
+          e.stopPropagation();
+          showThought("my mom delivers mail!!");
+        }}
+      />
+
       {/* My Home */}
       <button
         className={`${styles.hotspot} ${styles.home}`}
         onClick={(e) => {
           e.stopPropagation();
-          showThought(
-            "i live here with my family",
-            "26%",
-            "32%"
-          );
+          showThought("i live here with my family");
         }}
       />
 
@@ -44,11 +49,7 @@ export default function MapClient() {
         className={`${styles.hotspot} ${styles.lake}`}
         onClick={(e) => {
           e.stopPropagation();
-          showThought(
-            "we come here when it is warm",
-            "54%",
-            "35%"
-          );
+          showThought("we come here when it is warm");
         }}
       />
 
@@ -57,11 +58,7 @@ export default function MapClient() {
         className={`${styles.hotspot} ${styles.rock}`}
         onClick={(e) => {
           e.stopPropagation();
-          showThought(
-            "best barking spot",
-            "56%",
-            "12%"
-          );
+          showThought("best barking spot");
         }}
       />
 
@@ -70,11 +67,7 @@ export default function MapClient() {
         className={`${styles.hotspot} ${styles.ball}`}
         onClick={(e) => {
           e.stopPropagation();
-          showThought(
-            "hey! that is my ball!!",
-            "70%",
-            "30%"
-          );
+          showThought("hey! that is my ball!!");
         }}
       />
 
@@ -83,11 +76,7 @@ export default function MapClient() {
         className={`${styles.hotspot} ${styles.girlfriend}`}
         onClick={(e) => {
           e.stopPropagation();
-          showThought(
-            "girlfriend what are you doing there?!",
-            "80%",
-            "25%"
-          );
+          showThought("girlfriend what are you doing there?!");
         }}
       />
 
@@ -96,37 +85,25 @@ export default function MapClient() {
         className={`${styles.hotspot} ${styles.grandma}`}
         onClick={(e) => {
           e.stopPropagation();
-          showThought(
-            "i love her so much",
-            "18%",
-            "72%"
-          );
+          showThought("i love her so much");
         }}
       />
 
-     {/* garbage can */}
+      {/* garbage can */}
       <button
         className={`${styles.hotspot} ${styles.garbage}`}
         onClick={(e) => {
           e.stopPropagation();
-          showThought(
-            "the garbage truck comes here",
-            "35%",
-            "77%"
-          );
+          showThought("the garbage truck comes here");
         }}
       />
 
-   {/* sticks */}
+      {/* sticks */}
       <button
         className={`${styles.hotspot} ${styles.sticks}`}
         onClick={(e) => {
           e.stopPropagation();
-          showThought(
-            "these are important sticks",
-            "55%",
-            "57%"
-          );
+          showThought("these are important sticks");
         }}
       />
 
@@ -135,11 +112,7 @@ export default function MapClient() {
         className={`${styles.hotspot} ${styles.thomashouse}`}
         onClick={(e) => {
           e.stopPropagation();
-          showThought(
-            "thomas lives here",
-            "18%",
-            "32%"
-          );
+          showThought("thomas lives here");
         }}
       />
 
@@ -148,11 +121,7 @@ export default function MapClient() {
         className={`${styles.hotspot} ${styles.woods}`}
         onClick={(e) => {
           e.stopPropagation();
-          showThought(
-            "sometimes we look for blueberries here",
-            "22%",
-            "15%"
-          );
+          showThought("sometimes we look for blueberries here");
         }}
       />
 
@@ -161,23 +130,18 @@ export default function MapClient() {
         className={`${styles.hotspot} ${styles.vet}`}
         onClick={(e) => {
           e.stopPropagation();
-          showThought(
-            "they help dogs here",
-            "78%",
-            "72%"
-          );
+          showThought("they help dogs here");
         }}
       />
 
       {thought && (
-        <div
-          className={styles.thought}
-          style={{
-            left: thought.x,
-            top: thought.y,
-          }}
-        >
-          {thought.text}
+        <div className={styles.noteOverlay}>
+          <div
+            className={styles.thought}
+            onClick={(e) => e.stopPropagation()}
+          >
+            {thought}
+          </div>
         </div>
       )}
     </div>
